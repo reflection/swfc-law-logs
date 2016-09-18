0768bd32656532f4f25ca8c715a7bfeb1156594a
Samuel Chou <sam.chou@windystudios.com>
2016-09-17 19:57:54 -0700

    Add reaction logs

--- process_json.py
+++ process_json.py
@@ -2,6 +2,7 @@ import json
 
 actions = {}
 charges = {}
+reactions = {}
 
 with open('logs/law_battle.json', 'r') as f:
   for line in f:
@@ -19,6 +20,14 @@ with open('logs/law_battle.json', 'r') as f:
 
 out = []
 
+with open('logs/law_results.json', 'r') as f:
+  for line in f:
+    d = json.loads(line)
+
+    for h in d['history']:
+      if h['action'] not in reactions and 'date' in h:
+        reactions[h['action']] = h['date']
+
 for action, ts in actions.items():
   action = action.replace('<color=#35FC00>', '')
   action = action.replace('<color=#FF7800>', '')
@@ -28,6 +37,13 @@ for action, ts in actions.items():
 for c_json, ts in charges.items():
   c = json.loads(c_json)
   out.append((ts, 'charges: hp: {}, main_cannon: {}, sub_cannon: {}, shield: {}'.format(c['hp_charge'], c['main_cannon_charge'], c['sub_cannon_charge'], c['shield_charge'])))
+
+for action, ts in reactions.items():
+  action = h['action']
+  action = action.replace('<color=#35FC00>', '')
+  action = action.replace('<color=#FF7800>', '')
+  action = action.replace('</color>', '')
+  out.append((ts, 'action: {}'.format(action)))
 
 for ts, event in sorted(out, key=lambda x:-x[0]):
   print('{}:{:02d} left  {}'.format(int(ts / 60), ts % 60, event))
---
+++ retrieve_reaction_logs.py
@@ -0,0 +1,33 @@
+import json
+import requests
+from time import sleep
+from urllib.parse import urlencode
+
+h_post = {
+  'Accept-Language': 'en-us',
+  'Connection': 'keep-alive',
+  'Proxy-Connection': 'keep-alive',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'User-Agent': 'SWFC-U/n/4.3.14/[iPhone OS 9.3.3]/[iPad5,3]',
+  'X-Unity-Version': '4.7.2f1'
+}
+# Insert values for your current swfc session
+q = {
+  'PHPSESSID': '',
+  'opensocial_viewer_id': ''
+}
+data = {
+  'version': 43,
+  'division': 3,
+  'section': 1,
+  'turn': 1
+}
+
+url = 'http://amw.konaminet.jp/amw/naboo/api/guild/event/warship_combat/init?' + urlencode(q)
+
+with open('logs/law_results.json', 'wb') as f:
+  while True:
+    r = requests.post(url, data=data, headers=h_post, allow_redirects=True)
+    f.write(r.content)
+    f.write('\n'.encode('utf-8'))
+    sleep(1)
