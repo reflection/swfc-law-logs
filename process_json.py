d5577fbc82bf9c1ed4555fdcc8ea3d44709d5d65
Samuel Chou <sam.chou@windystudios.com>
2016-10-05 00:10:24 -0700

    Trim logs to boards and add combat logs

--- process_json.py
+++ process_json.py
@@ -9,39 +9,69 @@ with open('logs/law_battle.json', 'r') as f:
     d = json.loads(line)
 
     for h in d['history']:
-      if h['action'] not in actions and 'date' in h:
-        actions[h['action']] = d['base_info']['turn_resttime'] - h['date']
+      if h['action'].find('charged') > -1:
+        continue
+      if h['action'].find('launch') > -1:
+        continue
+      if h['action'].find('issued an order') > -1:
+        continue
+      if h['action'].find('initiated') > -1:
+        continue
 
-    c = d['action_result_turn_info']
-    del c['vehicle_charge']
-    c_json = json.dumps(c)
-    if c_json not in charges:
-      charges[c_json] = d['base_info']['turn_resttime']
+      action = h['action']
+      idx = action.find('... (Received')
+      if idx > -1:
+        action = action[:idx]
+      idx = action.find('successful!')
+      if idx > -1:
+        action = action[:idx+10]
+      if action not in actions and 'date' in h:
+        actions[action] = d['base_info']['turn_resttime'] - h['date']
 
     if int(d['abordage_cnt']) > boards:
       boards = int(d['abordage_cnt'])
 
 out = []
+legion = ''
+opponent = ''
+combat_logs = []
 
 with open('logs/law_results.json', 'r') as f:
   for line in f:
     d = json.loads(line)
 
+    legion = d['my_guild_info']['name']
+    opponent = d['enemy_guild_info']['guild_info']['name']
+
     for h in d['history']:
-      if h['action'] not in actions and 'date' in h:
-        actions[h['action']] = h['date']
+      action = h['action']
+      idx = action.find('... (Received')
+      if idx > -1:
+        action = action[:idx]
+      idx = action.find('!')
+      if idx > -1:
+        action = action[:idx]
+
+      if action not in actions and 'date' in h:
+        actions[action] = h['date']
+
+    for c in d['combat_log']:
+      if c['is_owner']:
+        combat_logs.append('Legion: {}'.format(' '.join(c['texts'])))
+      else:
+        combat_logs.append('Opponent: {}'.format(' '.join(c['texts'])))
 
 for action, ts in actions.items():
   action = action.replace('<color=#35FC00>', '')
   action = action.replace('<color=#FF7800>', '')
   action = action.replace('</color>', '')
-  out.append((ts, 'action: {}'.format(action)))
-
-for c_json, ts in charges.items():
-  c = json.loads(c_json)
-  out.append((ts, 'charges: hp: {}, main_cannon: {}, sub_cannon: {}, shield: {}'.format(c['hp_charge'], c['main_cannon_charge'], c['sub_cannon_charge'], c['shield_charge'])))
+  out.append((ts, '{}'.format(action)))
 
 for ts, event in sorted(out, key=lambda x:-x[0]):
-  print('{}:{:02d} left  {}'.format(int(ts / 60), ts % 60, event))
+  print('{}:{:02d} - {}'.format(int(ts / 60), ts % 60, event))
 
-print ('total boards: {}'.format(boards))
+print('total boards: {}\n\n'.format(boards))
+print('{} vs {} combat logs'.format(legion, opponent))
+for c in combat_logs:
+  print(c)
+  print('\n')
